8fc0768c9496259109eed3ad22907e91e0939743
Michael Stoelting <ms@reflect-media.de>
2020-12-20 18:58:29 +0100

    start to template the designdraft

---
+++ typo3_src-10.4.10/fileadmin/js/custom.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    /* Navbar scroll function */
+   if($(window).width() > 992) {
+       $(window).scroll(function() {
+           if($(this).scrollTop() > 40) {
+               $('.navbar').addClass('fixed-top');
+           } else {
+               $('.navbar').removeClass('fixed-top');
+           }
+       })
+   }
+
+   $('.mini-blog-article').click(function() {
+       $('.blog-overlay').addClass('show');
+       $('.blog-detail').addClass('show');
+   });
+
+   $('.blog-detail-closing').click(function() {
+        $('.blog-overlay').removeClass('show');
+        $('.blog-detail').removeClass('show');
+   });
+
+   $('.sidebar-title').click(function() {
+       $(this).parent().toggleClass('active');
+
+       //$('div[class^="sidebar-item"]:not(.active)').
+   })
+});
