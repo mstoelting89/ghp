a68f002999193a83b2e1447461ff5517fe4d4da3
123429 <u95803079@infong-eu68.kundenserver.de>
2020-12-25 19:21:23 +0100

    implementing Breadcrump and setup.typoscript file

--- typo3/typo3temp/var/cache/code/fluid_template/Standard_action_index_9e1b4c1959a7152a3a53af3bc023840be6135c3e.php
+++ typo3/typo3temp/var/cache/code/fluid_template/Standard_action_index_9e1b4c1959a7152a3a53af3bc023840be6135c3e.php
@@ -1,6 +1,6 @@
 <?php
 
-class Standard_action_index_ffea23185d8a788521666c8de64870043401f937 extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate {
+class Standard_action_index_9e1b4c1959a7152a3a53af3bc023840be6135c3e extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate {
 
 public function getLayoutName(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
 $self = $this;
@@ -36,30 +36,30 @@ $output0 = '';
 
 $output0 .= '
 <header>
+    ';
+// Rendering ViewHelper TYPO3\CMS\Fluid\ViewHelpers\DebugViewHelper
+$renderChildrenClosure2 = function() use ($renderingContext, $self) {
+return $renderingContext->getVariableProvider()->getAll();
+};
+$arguments1 = array();
+$arguments1['title'] = NULL;
+$arguments1['maxDepth'] = 8;
+$arguments1['plainText'] = false;
+$arguments1['ansiColors'] = false;
+$arguments1['inline'] = false;
+$arguments1['blacklistedClassNames'] = NULL;
+$arguments1['blacklistedPropertyNames'] = NULL;
+
+$output0 .= TYPO3\CMS\Fluid\ViewHelpers\DebugViewHelper::renderStatic($arguments1, $renderChildrenClosure2, $renderingContext);
+
+$output0 .= '
     <!--<div class="header-content" style="background-image: url(';
-$array1 = array (
+$array4 = array (
 );
-$output0 .= call_user_func_array( function ($var) { return (is_string($var) || (is_object($var) && method_exists($var, '__toString')) ? htmlspecialchars((string) $var, ENT_QUOTES) : $var); }, [$renderingContext->getVariableProvider()->getByPath('image', $array1)]);
+$output0 .= call_user_func_array( function ($var) { return (is_string($var) || (is_object($var) && method_exists($var, '__toString')) ? htmlspecialchars((string) $var, ENT_QUOTES) : $var); }, [$renderingContext->getVariableProvider()->getByPath('image', $array4)]);
 
 $output0 .= ')">-->
     <div class="header-content header-bg-image">
-        ';
-// Rendering ViewHelper TYPO3\CMS\Fluid\ViewHelpers\DebugViewHelper
-$renderChildrenClosure3 = function() use ($renderingContext, $self) {
-return $renderingContext->getVariableProvider()->getAll();
-};
-$arguments2 = array();
-$arguments2['title'] = NULL;
-$arguments2['maxDepth'] = 8;
-$arguments2['plainText'] = false;
-$arguments2['ansiColors'] = false;
-$arguments2['inline'] = false;
-$arguments2['blacklistedClassNames'] = NULL;
-$arguments2['blacklistedPropertyNames'] = NULL;
-
-$output0 .= TYPO3\CMS\Fluid\ViewHelpers\DebugViewHelper::renderStatic($arguments2, $renderChildrenClosure3, $renderingContext);
-
-$output0 .= '
         <nav class="navbar navbar-expand-lg navbar-light">
             <a class="navbar-brand" href="#">
                 ';
@@ -111,16 +111,12 @@ $output0 .= isset($arguments11['value']) ? $arguments11['value'] : $renderChildr
 $output0 .= '
         </div>
     </div>
-
-</header>
-
-<main>
-    <div class="container">
+    <div class="breadcrumb">
         ';
 // Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
 $renderChildrenClosure15 = function() use ($renderingContext, $self) {
 $array16 = array (
-);return $renderingContext->getVariableProvider()->getByPath('contentMain', $array16);
+);return $renderingContext->getVariableProvider()->getByPath('menuBreadcrumb', $array16);
 };
 $arguments14 = array();
 $arguments14['value'] = NULL;
@@ -129,18 +125,35 @@ $output0 .= isset($arguments14['value']) ? $arguments14['value'] : $renderChildr
 
 $output0 .= '
     </div>
+</header>
 
-    <div class="sidebars">
+<main>
+    <div class="container">
         ';
 // Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
 $renderChildrenClosure18 = function() use ($renderingContext, $self) {
 $array19 = array (
-);return $renderingContext->getVariableProvider()->getByPath('sidebars', $array19);
+);return $renderingContext->getVariableProvider()->getByPath('contentMain', $array19);
 };
 $arguments17 = array();
 $arguments17['value'] = NULL;
 
 $output0 .= isset($arguments17['value']) ? $arguments17['value'] : $renderChildrenClosure18();
+
+$output0 .= '
+    </div>
+
+    <div class="sidebars">
+        ';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
+$renderChildrenClosure21 = function() use ($renderingContext, $self) {
+$array22 = array (
+);return $renderingContext->getVariableProvider()->getByPath('sidebars', $array22);
+};
+$arguments20 = array();
+$arguments20['value'] = NULL;
+
+$output0 .= isset($arguments20['value']) ? $arguments20['value'] : $renderChildrenClosure21();
 
 $output0 .= '
     </div>
@@ -151,24 +164,9 @@ $output0 .= '
     <div class="sub-text-footer pt-3 pb-3">
         ';
 // Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
-$renderChildrenClosure21 = function() use ($renderingContext, $self) {
-$array22 = array (
-);return $renderingContext->getVariableProvider()->getByPath('footerLabel', $array22);
-};
-$arguments20 = array();
-$arguments20['value'] = NULL;
-
-$output0 .= isset($arguments20['value']) ? $arguments20['value'] : $renderChildrenClosure21();
-
-$output0 .= '
-    </div>
-
-    <div class="partners">
-        ';
-// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
 $renderChildrenClosure24 = function() use ($renderingContext, $self) {
 $array25 = array (
-);return $renderingContext->getVariableProvider()->getByPath('footerPartners', $array25);
+);return $renderingContext->getVariableProvider()->getByPath('footerLabel', $array25);
 };
 $arguments23 = array();
 $arguments23['value'] = NULL;
@@ -176,19 +174,34 @@ $arguments23['value'] = NULL;
 $output0 .= isset($arguments23['value']) ? $arguments23['value'] : $renderChildrenClosure24();
 
 $output0 .= '
-
     </div>
-    <div class="secondary-menu">
+
+    <div class="partners">
         ';
 // Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
 $renderChildrenClosure27 = function() use ($renderingContext, $self) {
 $array28 = array (
-);return $renderingContext->getVariableProvider()->getByPath('footerSecondaryMenu', $array28);
+);return $renderingContext->getVariableProvider()->getByPath('footerPartners', $array28);
 };
 $arguments26 = array();
 $arguments26['value'] = NULL;
 
 $output0 .= isset($arguments26['value']) ? $arguments26['value'] : $renderChildrenClosure27();
+
+$output0 .= '
+
+    </div>
+    <div class="secondary-menu">
+        ';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
+$renderChildrenClosure30 = function() use ($renderingContext, $self) {
+$array31 = array (
+);return $renderingContext->getVariableProvider()->getByPath('footerSecondaryMenu', $array31);
+};
+$arguments29 = array();
+$arguments29['value'] = NULL;
+
+$output0 .= isset($arguments29['value']) ? $arguments29['value'] : $renderChildrenClosure30();
 
 $output0 .= '
 
