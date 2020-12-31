8a9eea76bca1df0dcadeb405b4fab197671af0b6
123429 <u95803079@infong-eu68.kundenserver.de>
2020-12-31 14:48:41 +0100

    server update

--- typo3/typo3temp/var/cache/code/cache_core/pseudo-sites.php
+++
@@ -1,3 +0,0 @@
-<?php
-[[{"languageId":0,"title":"Default","flag":""},{"languageId":1,"title":"Englisch","iso-639-1":"en","flag":"flags-gb","enabled":true}],[],[{"uid":19},{"uid":16},{"uid":1},{"uid":14}]]
-#
