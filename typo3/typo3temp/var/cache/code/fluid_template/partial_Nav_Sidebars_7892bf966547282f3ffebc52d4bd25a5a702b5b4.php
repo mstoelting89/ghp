674722881513cb52c5fd2b5617e87be8399d2881
mstoelting89 <michaelstoelting@gmail.com>
2021-01-10 15:38:45 +0100

    server update

---
+++ typo3/typo3temp/var/cache/code/fluid_template/partial_Nav_Sidebars_7892bf966547282f3ffebc52d4bd25a5a702b5b4.php
@@ -0,0 +1,89 @@
+<?php
+
+class partial_Nav_Sidebars_7892bf966547282f3ffebc52d4bd25a5a702b5b4 extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate {
+
+public function getLayoutName(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+return (string) '';
+}
+public function hasLayout() {
+return FALSE;
+}
+public function addCompiledNamespaces(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$renderingContext->getViewHelperResolver()->addNamespaces(array (
+  'core' => 
+  array (
+    0 => 'TYPO3\\CMS\\Core\\ViewHelpers',
+  ),
+  'f' => 
+  array (
+    0 => 'TYPO3Fluid\\Fluid\\ViewHelpers',
+    1 => 'TYPO3\\CMS\\Fluid\\ViewHelpers',
+  ),
+  'formvh' => 
+  array (
+    0 => 'TYPO3\\CMS\\Form\\ViewHelpers',
+  ),
+));
+}
+
+/**
+ * Main Render function
+ */
+public function render(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+$output0 = '';
+
+$output0 .= '
+
+';
+// Rendering ViewHelper TYPO3\CMS\Fluid\ViewHelpers\DebugViewHelper
+$renderChildrenClosure2 = function() use ($renderingContext, $self) {
+$array3 = array (
+);return $renderingContext->getVariableProvider()->getByPath('sidebars', $array3);
+};
+$arguments1 = array();
+$arguments1['title'] = NULL;
+$arguments1['maxDepth'] = 8;
+$arguments1['plainText'] = false;
+$arguments1['ansiColors'] = false;
+$arguments1['inline'] = false;
+$arguments1['blacklistedClassNames'] = NULL;
+$arguments1['blacklistedPropertyNames'] = NULL;
+
+$output0 .= TYPO3\CMS\Fluid\ViewHelpers\DebugViewHelper::renderStatic($arguments1, $renderChildrenClosure2, $renderingContext);
+
+$output0 .= '
+<div class="sidebars">
+
+    ';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\ForViewHelper
+$renderChildrenClosure5 = function() use ($renderingContext, $self) {
+return '
+        <div class="sidebar-item">
+        </div>
+
+    ';
+};
+$arguments4 = array();
+$arguments4['each'] = NULL;
+$arguments4['as'] = NULL;
+$arguments4['key'] = NULL;
+$arguments4['reverse'] = false;
+$arguments4['iteration'] = NULL;
+$array6 = array (
+);$arguments4['each'] = $renderingContext->getVariableProvider()->getByPath('sidebars', $array6);
+$array7 = array (
+);$arguments4['as'] = $renderingContext->getVariableProvider()->getByPath('sidebarItem', $array7);
+
+$output0 .= TYPO3Fluid\Fluid\ViewHelpers\ForViewHelper::renderStatic($arguments4, $renderChildrenClosure5, $renderingContext);
+
+$output0 .= '
+</div>';
+
+return $output0;
+}
+
+
+}
+#
