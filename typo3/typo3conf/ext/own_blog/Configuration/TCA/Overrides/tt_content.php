6f99aa1cd47a829b453b1ad503a655ed87d93941
Michael Stoelting <ms@reflect-media.de>
2021-01-11 08:35:31 +0100

    try to add a new content element type

--- typo3/typo3conf/ext/own_blog/Configuration/TCA/Overrides/tt_content.php
+++ typo3/typo3conf/ext/own_blog/Configuration/TCA/Overrides/tt_content.php
@@ -24,4 +24,36 @@ $iconRegistry->registerIcon(
     ['name' => 'external-link-alt']
 );
 
+$iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+
+// use same identifier as used in TSconfig for icon
+$iconRegistry->registerIcon(
+// use same identifier as used in TSconfig for icon
+    'examples_newcontentelement',
+    \TYPO3\CMS\Core\Imaging\IconProvider\FontawesomeIconProvider::class,
+    // font-awesome identifier ('external-link-square')
+    ['name' => 'external-link-alt']
+);
+
+// Configure the default backend fields for the content element
+$GLOBALS['TCA']['tt_content']['types']['examples_newcontentelement'] = [
+    'showitem' => '
+         --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+            --palette--;;general,
+            header; Internal title (not displayed),
+            bodytext;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:bodytext_formlabel,
+         --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+            --palette--;;hidden,
+            --palette--;;access,
+      ',
+    'columnsOverrides' => [
+        'bodytext' => [
+            'config' => [
+                'enableRichtext' => true,
+                'richtextConfiguration' => 'default',
+            ],
+        ],
+    ],
+];
+
 
