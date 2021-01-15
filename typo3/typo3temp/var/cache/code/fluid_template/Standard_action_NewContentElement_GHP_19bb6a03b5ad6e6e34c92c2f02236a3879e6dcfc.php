29210e92e9e64d5e7c8a547d6b5b9827b20cbbf0
mstoelting89 <michaelstoelting@gmail.com>
2021-01-15 23:08:18 +0100

    server update

---
+++ typo3/typo3temp/var/cache/code/fluid_template/Standard_action_NewContentElement_GHP_19bb6a03b5ad6e6e34c92c2f02236a3879e6dcfc.php
@@ -0,0 +1,60 @@
+<?php
+
+class Standard_action_NewContentElement_GHP_19bb6a03b5ad6e6e34c92c2f02236a3879e6dcfc extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate {
+
+public function getLayoutName(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+return (string) '';
+}
+public function hasLayout() {
+return FALSE;
+}
+public function addCompiledNamespaces(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$renderingContext->getViewHelperResolver()->addNamespaces(array (
+  'core' => 
+  array (
+    0 => 'TYPO3\\CMS\\Core\\ViewHelpers',
+  ),
+  'f' => 
+  array (
+    0 => 'TYPO3Fluid\\Fluid\\ViewHelpers',
+    1 => 'TYPO3\\CMS\\Fluid\\ViewHelpers',
+  ),
+  'formvh' => 
+  array (
+    0 => 'TYPO3\\CMS\\Form\\ViewHelpers',
+  ),
+));
+}
+
+/**
+ * Main Render function
+ */
+public function render(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+$output0 = '';
+
+$output0 .= '
+
+<h2>Das ist der Output unter fileadmin aber jetzt mit dem richtig TemplateNamen</h2>
+<div>';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
+$renderChildrenClosure2 = function() use ($renderingContext, $self) {
+$array3 = array (
+);return $renderingContext->getVariableProvider()->getByPath('data.bodytext', $array3);
+};
+$arguments1 = array();
+$arguments1['value'] = NULL;
+
+$output0 .= isset($arguments1['value']) ? $arguments1['value'] : $renderChildrenClosure2();
+
+$output0 .= '</div>
+
+';
+
+return $output0;
+}
+
+
+}
+#
