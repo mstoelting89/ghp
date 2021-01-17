f9ea0d48b50fc7a59ae9f8459064f55a0bee6a55
mstoelting89 <michaelstoelting@gmail.com>
2021-01-17 22:12:46 +0100

    server update

---
+++ typo3/typo3temp/var/cache/code/fluid_template/partial_Nav_Navbar_3370f7559267a2246830fe6dd1007c497654ab45.php
@@ -0,0 +1,86 @@
+<?php
+
+class partial_Nav_Navbar_3370f7559267a2246830fe6dd1007c497654ab45 extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate {
+
+public function getLayoutName(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+return (string) '';
+}
+public function hasLayout() {
+return FALSE;
+}
+public function addCompiledNamespaces(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$renderingContext->getViewHelperResolver()->addNamespaces(array (
+  'core' => 
+  array (
+    0 => 'TYPO3\\CMS\\Core\\ViewHelpers',
+  ),
+  'f' => 
+  array (
+    0 => 'TYPO3Fluid\\Fluid\\ViewHelpers',
+    1 => 'TYPO3\\CMS\\Fluid\\ViewHelpers',
+  ),
+  'formvh' => 
+  array (
+    0 => 'TYPO3\\CMS\\Form\\ViewHelpers',
+  ),
+  'n' => 
+  array (
+    0 => 'GeorgRinger\\News\\ViewHelpers',
+  ),
+));
+}
+
+/**
+ * Main Render function
+ */
+public function render(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+$output0 = '';
+
+$output0 .= '
+
+<nav class="navbar navbar-expand-lg navbar-light">
+    <a class="navbar-brand" href="#">
+        ';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
+$renderChildrenClosure2 = function() use ($renderingContext, $self) {
+$array3 = array (
+);return $renderingContext->getVariableProvider()->getByPath('logoScroll', $array3);
+};
+$arguments1 = array();
+$arguments1['value'] = NULL;
+
+$output0 .= isset($arguments1['value']) ? $arguments1['value'] : $renderChildrenClosure2();
+
+$output0 .= '
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+
+        ';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
+$renderChildrenClosure5 = function() use ($renderingContext, $self) {
+$array6 = array (
+);return $renderingContext->getVariableProvider()->getByPath('menuPrimary', $array6);
+};
+$arguments4 = array();
+$arguments4['value'] = NULL;
+
+$output0 .= isset($arguments4['value']) ? $arguments4['value'] : $renderChildrenClosure5();
+
+$output0 .= '
+
+    </div>
+</nav>
+';
+
+return $output0;
+}
+
+
+}
+#
