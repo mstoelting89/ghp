3b799d4da485869106cc6f0cf79dfcd6e84a3a50
mstoelting89 <michaelstoelting@gmail.com>
2021-01-17 20:01:22 +0100

    Server update

---
+++ typo3/typo3temp/var/cache/code/fluid_template/Standard_action_Header_83f92645225886191c36b8b6a63be1886ab7734a.php
@@ -0,0 +1,251 @@
+<?php
+
+class Standard_action_Header_83f92645225886191c36b8b6a63be1886ab7734a extends \TYPO3Fluid\Fluid\Core\Compiler\AbstractCompiledTemplate {
+
+public function getLayoutName(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+return (string) 'Default';
+}
+public function hasLayout() {
+return TRUE;
+}
+public function addCompiledNamespaces(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$renderingContext->getViewHelperResolver()->addNamespaces(array (
+  'core' => 
+  array (
+    0 => 'TYPO3\\CMS\\Core\\ViewHelpers',
+  ),
+  'f' => 
+  array (
+    0 => 'TYPO3Fluid\\Fluid\\ViewHelpers',
+    1 => 'TYPO3\\CMS\\Fluid\\ViewHelpers',
+  ),
+  'formvh' => 
+  array (
+    0 => 'TYPO3\\CMS\\Form\\ViewHelpers',
+  ),
+  'n' => 
+  array (
+    0 => 'GeorgRinger\\News\\ViewHelpers',
+  ),
+));
+}
+
+/**
+ * section header
+ */
+public function section_594fd1615a341c77829e83ed988f137e1ba96231(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+$output0 = '';
+
+$output0 .= '
+    <header>
+        <!--<div class="header-content" style="background-image: url(';
+$array1 = array (
+);
+$output0 .= call_user_func_array( function ($var) { return (is_string($var) || (is_object($var) && method_exists($var, '__toString')) ? htmlspecialchars((string) $var, ENT_QUOTES) : $var); }, [$renderingContext->getVariableProvider()->getByPath('image', $array1)]);
+
+$output0 .= ')">-->
+        <div class="header-content header-bg-image">
+            ';
+// Rendering ViewHelper TYPO3\CMS\Fluid\ViewHelpers\RenderViewHelper
+$renderChildrenClosure3 = function() use ($renderingContext, $self) {
+return NULL;
+};
+$arguments2 = array();
+$arguments2['section'] = NULL;
+$arguments2['partial'] = NULL;
+$arguments2['delegate'] = NULL;
+$arguments2['renderable'] = NULL;
+$arguments2['arguments'] = array (
+);
+$arguments2['optional'] = false;
+$arguments2['default'] = NULL;
+$arguments2['contentAs'] = NULL;
+$arguments2['debug'] = true;
+$arguments2['partial'] = 'Nav/Navbar';
+$arguments2['arguments'] = $renderingContext->getVariableProvider()->getAll();
+
+$output0 .= TYPO3\CMS\Fluid\ViewHelpers\RenderViewHelper::renderStatic($arguments2, $renderChildrenClosure3, $renderingContext);
+
+$output0 .= '
+
+
+            <div class="row header-title align-items-center">
+                <div class="col-4 col-md-4 col-lg-4 logo-align">
+                    ';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
+$renderChildrenClosure6 = function() use ($renderingContext, $self) {
+$array7 = array (
+);return $renderingContext->getVariableProvider()->getByPath('logo', $array7);
+};
+$arguments5 = array();
+$arguments5['value'] = NULL;
+
+$output0 .= isset($arguments5['value']) ? $arguments5['value'] : $renderChildrenClosure6();
+
+$output0 .= '
+                </div>
+                <div class="col-12 col-md-12 col-lg-8">
+                    ';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
+$renderChildrenClosure9 = function() use ($renderingContext, $self) {
+$array10 = array (
+);return $renderingContext->getVariableProvider()->getByPath('contentTrailer', $array10);
+};
+$arguments8 = array();
+$arguments8['value'] = NULL;
+
+$output0 .= isset($arguments8['value']) ? $arguments8['value'] : $renderChildrenClosure9();
+
+$output0 .= '
+                </div>
+            </div>
+        </div>
+        <!--
+        <div class="breadcrumb-container">
+            ';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
+$renderChildrenClosure12 = function() use ($renderingContext, $self) {
+$array13 = array (
+);return $renderingContext->getVariableProvider()->getByPath('menuBreadcrumb', $array13);
+};
+$arguments11 = array();
+$arguments11['value'] = NULL;
+
+$output0 .= isset($arguments11['value']) ? $arguments11['value'] : $renderChildrenClosure12();
+
+$output0 .= '
+        </div>
+        -->
+    </header>
+';
+
+return $output0;
+}
+/**
+ * Main Render function
+ */
+public function render(\TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface $renderingContext) {
+$self = $this;
+$output14 = '';
+
+$output14 .= '
+
+';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\LayoutViewHelper
+$renderChildrenClosure16 = function() use ($renderingContext, $self) {
+return NULL;
+};
+$arguments15 = array();
+$arguments15['name'] = NULL;
+$arguments15['name'] = 'Default';
+
+$output14 .= call_user_func_array( function ($var) { return (is_string($var) || (is_object($var) && method_exists($var, '__toString')) ? htmlspecialchars((string) $var, ENT_QUOTES) : $var); }, [NULL]);
+
+$output14 .= '
+
+';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\SectionViewHelper
+$renderChildrenClosure18 = function() use ($renderingContext, $self) {
+$output19 = '';
+
+$output19 .= '
+    <header>
+        <!--<div class="header-content" style="background-image: url(';
+$array20 = array (
+);
+$output19 .= call_user_func_array( function ($var) { return (is_string($var) || (is_object($var) && method_exists($var, '__toString')) ? htmlspecialchars((string) $var, ENT_QUOTES) : $var); }, [$renderingContext->getVariableProvider()->getByPath('image', $array20)]);
+
+$output19 .= ')">-->
+        <div class="header-content header-bg-image">
+            ';
+// Rendering ViewHelper TYPO3\CMS\Fluid\ViewHelpers\RenderViewHelper
+$renderChildrenClosure22 = function() use ($renderingContext, $self) {
+return NULL;
+};
+$arguments21 = array();
+$arguments21['section'] = NULL;
+$arguments21['partial'] = NULL;
+$arguments21['delegate'] = NULL;
+$arguments21['renderable'] = NULL;
+$arguments21['arguments'] = array (
+);
+$arguments21['optional'] = false;
+$arguments21['default'] = NULL;
+$arguments21['contentAs'] = NULL;
+$arguments21['debug'] = true;
+$arguments21['partial'] = 'Nav/Navbar';
+$arguments21['arguments'] = $renderingContext->getVariableProvider()->getAll();
+
+$output19 .= TYPO3\CMS\Fluid\ViewHelpers\RenderViewHelper::renderStatic($arguments21, $renderChildrenClosure22, $renderingContext);
+
+$output19 .= '
+
+
+            <div class="row header-title align-items-center">
+                <div class="col-4 col-md-4 col-lg-4 logo-align">
+                    ';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
+$renderChildrenClosure25 = function() use ($renderingContext, $self) {
+$array26 = array (
+);return $renderingContext->getVariableProvider()->getByPath('logo', $array26);
+};
+$arguments24 = array();
+$arguments24['value'] = NULL;
+
+$output19 .= isset($arguments24['value']) ? $arguments24['value'] : $renderChildrenClosure25();
+
+$output19 .= '
+                </div>
+                <div class="col-12 col-md-12 col-lg-8">
+                    ';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
+$renderChildrenClosure28 = function() use ($renderingContext, $self) {
+$array29 = array (
+);return $renderingContext->getVariableProvider()->getByPath('contentTrailer', $array29);
+};
+$arguments27 = array();
+$arguments27['value'] = NULL;
+
+$output19 .= isset($arguments27['value']) ? $arguments27['value'] : $renderChildrenClosure28();
+
+$output19 .= '
+                </div>
+            </div>
+        </div>
+        <!--
+        <div class="breadcrumb-container">
+            ';
+// Rendering ViewHelper TYPO3Fluid\Fluid\ViewHelpers\Format\RawViewHelper
+$renderChildrenClosure31 = function() use ($renderingContext, $self) {
+$array32 = array (
+);return $renderingContext->getVariableProvider()->getByPath('menuBreadcrumb', $array32);
+};
+$arguments30 = array();
+$arguments30['value'] = NULL;
+
+$output19 .= isset($arguments30['value']) ? $arguments30['value'] : $renderChildrenClosure31();
+
+$output19 .= '
+        </div>
+        -->
+    </header>
+';
+return $output19;
+};
+$arguments17 = array();
+$arguments17['name'] = NULL;
+$arguments17['name'] = 'header';
+
+$output14 .= NULL;
+
+$output14 .= '
+';
+
+return $output14;
+}
+
+
+}
+#
