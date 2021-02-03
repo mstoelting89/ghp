6b6aebf42a2d1900cc9c000dc1e569da119182c6
Michael Stoelting <ms@reflect-media.de>
2021-02-03 12:20:00 +0100

    update folder structure

--- typo3/uploads/tx_extensionbuilder/backups/pi_ghp/2021-01-12-1610437337/ext_tables.sql
+++
@@ -1,6 +0,0 @@
-#
-# Table structure for table 'tx_pighp_domain_model_guitarheartsproject'
-#
-CREATE TABLE tx_pighp_domain_model_guitarheartsproject (
-
-);
--- typo3/uploads/tx_extensionbuilder/backups/pi_ghp/2021-01-12-1610437373/ext_tables.sql
+++
@@ -1,8 +0,0 @@
-#
-# Table structure for table 'tx_pighp_domain_model_guitarheartsproject'
-#
-CREATE TABLE tx_pighp_domain_model_guitarheartsproject (
-
-	name varchar(255) DEFAULT '' NOT NULL
-
-);
